3d9614b7b64ba1b75df730b106134d28ddecb0c6
birdpp55 <133236672+birdpp55@users.noreply.github.com>
2025-10-30 22:21:35 +0800

    Update gacha-system.js

--- fgo/js/gacha-system.js
+++ fgo/js/gacha-system.js
@@ -1,6 +1,29 @@
 class GachaSystem {
     constructor() {
         this.cardData = cardData; // 使用外部cards.js中的数据
+        this.preloadedImages = new Set(); // 存储已预加载的图片
+        this.preloadImages(); // 初始化时预加载图片
+    }
+
+    // 预加载所有卡片图片
+    preloadImages() {
+        // 预加载所有卡片的class_image和image
+        this.cardData.forEach(card => {
+            this.preloadImage(card.class_image);
+            this.preloadImage(card.image);
+        });
+        
+        // 预加载庆祝图片
+        this.preloadImage('../images/pic/cele.jpg');
+    }
+
+    // 预加载单个图片
+    preloadImage(src) {
+        if (this.preloadedImages.has(src)) return;
+        
+        const img = new Image();
+        img.src = src;
+        this.preloadedImages.add(src);
     }
 
     getRandomCard() {
